51ce6d4e457f0ef53d455ed7309fa479432b601f
Praseesh <praseeshprasee@gmail.com>
2024-04-27 18:49:04 +0530

    signup views

--- login/loginapp1/views.py
+++ login/loginapp1/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import User
 
 def user_login(request):
     return render(request, 'login.html')
 
 
 def user_signup(request):
+    if request.session['username'] is not None:
+        return redirect(home)
+    
     username= request.POST.get("username")
     pwd = request.POST.get("password")
     name = request.POST.get('name')
     
-    if username is None or pwd is None or name is None:
+    if not (username and pwd and name):
         msg1 = "Bad request..! All field are required"
         return render(request, 'signup.html', {'msg':msg1})
     
-def home(reqeust):
+    user = User(username=username,password=pwd, name=name)
+    user.save()
+    return redirect("home")
+    
+    
+def user_login(request):
+    if request.method=='POST':
+        if request.session['username'] is not None:
+            return redirect(home)
+        username= request.POST.get("username")
+        pwd = request.POST.get("password")
+    
+
+        
+ 
+def home(request):
     
     pass
